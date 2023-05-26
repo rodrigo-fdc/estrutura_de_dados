4b3d9d660f910a08da98e991e3c1a79cf297e26b
Rodrigo Ferreira <rodrigoferreira.mkt@gmail.com>
2023-05-26 00:28:47 -0300

    Modelagem da classe Vetor

---
+++ src/listas/Vetor.java
@@ -0,0 +1,47 @@
+/*
+ * Implementação da classe para listar alunos de uma classe
+ * 
+ * A classe implementa os seguintes métodos/operações
+ * 
+ * 1 - Adicionar um aluno ao fim da lista
+ * 2 - Adicionar um aluno em uma dada posição
+ * 3 - Pega o aluno de dado posição
+ * 4 - Remove o aluno de dado posição
+ * 5 - Verifica se um aluno está armazenado
+ * 6 - Informa o número de alunos armazenados
+ * 
+ */
+
+package listas;
+
+public class Vetor {
+
+	//Declarando e iniciando um array de Aluno com capacidade de 100
+	private Aluno[] alunos = new Aluno[100];
+	
+	public void adiciona(Aluno aluno) {
+		//implementação
+	}
+	
+	public void adiciona(int posicao, Aluno aluno) {
+		//implementação
+	}
+	
+	public Aluno pega(int posicao) {
+		//implementação
+	}
+	
+	public void remove(int posicao) {
+		//implementação
+	}
+	public boolean contem(Aluno aluno) {
+		//implementação
+	}
+	public int tamanho() {
+		//implementação
+	}
+	
+	public String to.String() {
+		return Arrays.toString(alunos);
+	}
+}
