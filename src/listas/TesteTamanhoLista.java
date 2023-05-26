8a1a8fcf15e51e90c3968cc57b42a82ab0b17bf9
Rodrigo Ferreira <rodrigoferreira.mkt@gmail.com>
2023-05-26 08:25:09 -0300

    Testes

---
+++ src/listas/TesteTamanhoLista.java
@@ -0,0 +1,27 @@
+package listas;
+
+public class TesteTamanhoLista {
+
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno();
+		Aluno a2 = new Aluno();
+		Aluno a3 = new Aluno();
+		
+		a1.setNome("Rafael");
+		a1.setNome("Paulo");
+		
+		Vetor lista = new Vetor();
+		
+		lista.adiciona(a1);
+		lista.adiciona(a2);
+		
+		System.out.println(lista.tamanho());
+		
+		
+		lista.adiciona(a3);
+		
+		System.out.println(lista.tamanho());
+		
+	}
+
+}
